320c394c94ab7da274e41b8c45a900157d95aa90
CYetlanezi <cymr_31@hotmail.com>
2016-11-25 02:08:11 -0600

    sd
    asd

---
+++ Pixel.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package menesesramirezclaudia;
+
+/**
+ *
+ * @author hca
+ */
+public class Pixel {
+    private int rojo;
+    private int verde;
+    private int azul;
+     private int fi;
+     private int co;
+      
+    public Pixel(int r, int v, int b,int co,int fi){
+        this.rojo = r;
+        this.verde = v;
+        this.azul = b;
+        this.fi = fi;
+        this.co = co;
+    }
+    public void setRojo(int re){
+        this.rojo = re;
+    }
+    public int getRojo(){
+        return rojo;
+    }
+    public int getFila(){
+        return fi+1;
+    }
+    public int getColumna(){
+        return co+1;
+    }
+    public String toString(){
+        return ""+rojo+" "+verde+" "+azul+"\n";
+    }
+}
